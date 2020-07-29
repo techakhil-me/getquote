b559c62851ee13866f1b8e63f685faad87c73583
TechAkhil <43501564+techakhilc47@users.noreply.github.com>
2020-07-29 14:44:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function getQuote(){
+    fetch("https://type.fit/api/quotes", {
+            method: "GET",
+}).then(response => {return response.json()}).then(response=>{
+    // console.log(response);
+    let max = response.length;
+    let min = 0;
+    let k = Math.floor(Math.random()*(max-min))
+    console.log(response[k]);
+    var randomquote = response[k];
+    if (randomquote["author"]==null){
+        return getQuote();
+    };
+    document.getElementById("quote").innerHTML = randomquote["text"];
+    document.getElementById("author").innerHTML = `BY- ${randomquote["author"]}`;
+    document.getElementById("button").innerHTML = "GET ANOTHER QUOTE";
+})
+}
